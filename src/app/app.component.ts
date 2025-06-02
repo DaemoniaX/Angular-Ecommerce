331a402548107b2645eeb10b80ab441a90fc04ed
Antonin Marolleau <137926499+DaemoniaX@users.noreply.github.com>
2025-06-02 18:13:51 +0200

    Fix app.component.ts
    FIX 02/06/2025, the "About us & contact" that wasn't linked at the bottom of the page issue is now fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,42 @@
+// app.component.ts
+//FIX 02/06/2025, the "About us & contact" that wasn't linked at the bottom of the page issue is now fixed.
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from "./pages/products-list/products-list.component";
 
-@Component({
+@Component({//Method FlexBox
   selector: 'app-root',
   imports: [HeaderComponent, RouterOutlet],
   template: `
-    <app-header />
-    <router-outlet />
+    <app-header></app-header>
 
-    <footer class="bg-black text-white py-4 text-center">
+    <main class="content">
+      <router-outlet></router-outlet>
+    </main>
+
+    <footer class="footer bg-black text-white py-4 text-center">
       About us &nbsp; Contact
     </footer>
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+
+      .content {
+        flex: 1;
+      }
+
+      .footer {
+
+      }
+    `
+  ],
+  standalone: true
 })
-export class AppComponent {}
+export class AppComponent {
+  
+}
