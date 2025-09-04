69e9edb1bd87f46bf5b9a609789e68147fa83115
Antonin Marolleau <137926499+DaemoniaX@users.noreply.github.com>
2025-09-04 18:50:04 +0200

    Fix 04/09/2025
    button was bad => more highlight, cursor pointer

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,20 +1,18 @@
 import { Component, input, output } from '@angular/core';
+//Changelog 04/09/2025 @AntoninMarolleau : button was not really great => more highlight, cursor pointer
 
 @Component({
-  selector: 'app-button',
+  selector: 'app-primary-button',
   imports: [],
   template: `
-    <button
-      class=" text-black w-full px-5 py-2 rounded-xl shadow-md hover:bg-slate-200"
-      (click)="btnClicked.emit()"
-    >
-      <span class="text-md">{{ label() }}</span>
+    <button class="bg-red-500/75 text-black w-full border px-5 py-2 rounded shadow-md hover:opacity-67 cursor-pointer"(click)="buttonClicked.emit()">
+      {{ label() }}
     </button>
   `,
-  styles: ``,
+  styles: ``
 })
-export class ButtonComponent {
-  label = input<string>();
+export class PrimaryButtonComponent {
+  label = input('');
 
-  btnClicked = output();
+  buttonClicked = output();
 }
